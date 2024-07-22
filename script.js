437b3380c4ff239092c230a9571278f04c006d0a
udayram0558 <udayram0558u@gmail.com>
2024-07-22 16:45:22 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function colorChange(element, option){
+    const correctans = 'D'
+    if(option === correctans){
+         element.style.backgroundColor = '#12d941'
+    }else{
+         element.style.backgroundColor = '#FF4433'
+    }
+
+}
